7f4c05bacecac3c1481597ddc6f2f35406c25ec6
Shysh-Oleksandr <sasha.shysh23@gmail.com>
2022-03-11 11:40:59 +0200

    Initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center my-4 mx-auto text-4xl text-black font-bold">
+        Mnemory
+      </h1>
+    </div>
+  );
+}
+
+export default App;
