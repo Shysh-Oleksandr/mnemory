50971bc128a6e20705379255e307f3f05a69e54c
Shysh-Oleksandr <sasha.shysh23@gmail.com>
2022-03-14 16:40:09 +0200

    Made fetching images by keyword name.

--- src/components/termCard/TermKeywordImageChoice.tsx
+++ src/components/termCard/TermKeywordImageChoice.tsx
@@ -3,10 +3,20 @@ import { ITerm } from "./Term";
 import { useDispatch } from "react-redux";
 import { actionCreactors } from "../../state";
 import { bindActionCreators } from "redux";
+import { useForm } from "react-hook-form";
 
 type Props = { term: ITerm };
 
+const CLIENT_API = "gK52De2Tm_dL5o1IXKa9FROBAJ-LIYqR41xBdlg3X2k"; // Another one
+
 const TermKeywordImageChoice = ({ term }: Props) => {
+  const [searchedImages, setSearchedImages] = useState<string[]>([]);
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+  } = useForm();
   const dispatch = useDispatch();
   const { toggleTermKeywordImage, setTermKeywordImage } = bindActionCreators(
     actionCreactors,
@@ -16,6 +26,20 @@ const TermKeywordImageChoice = ({ term }: Props) => {
   const foundKeyword = term.descriptionKeywords.find(
     (keyword) => keyword.imageChecked
   );
+
+  function fetchKeywordImages() {
+    const query: string = getValues("imageQuery");
+    fetch(
+      `https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=ZiayMmOG_HV-OOVULOC8bxjPCyBlJO23BKeXIl9zh-M`
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        let foundImages = data.results.map((result: any) => result.urls.raw);
+        setSearchedImages(foundImages);
+      });
+  }
 
   function getFileImageUrl(e: React.ChangeEvent<HTMLInputElement>): string {
     let file = e.target.files![0];
@@ -34,11 +58,7 @@ const TermKeywordImageChoice = ({ term }: Props) => {
 
   return (
     <div className="keyword-image-choice bg-slate-900 text-slate-100 rounded-lg py-6 px-6">
-      <form
-        onSubmit={() => {
-          console.log("submit");
-        }}
-      >
+      <form onSubmit={handleSubmit(fetchKeywordImages)}>
         <div className="flex items-center">
           <div className="relative ">
             <input
@@ -48,6 +68,7 @@ const TermKeywordImageChoice = ({ term }: Props) => {
               placeholder={foundKeyword.keyword}
               className="keyword-image-input term-input"
               id={`term-${term.id}-${foundKeyword.id}`}
+              {...register("imageQuery", { required: true })}
             />
             <button
               className="absolute right-0 top-1/2 -translate-y-1/2 hover:text-orange-500 text-orange-400 transition-colors"
@@ -80,7 +101,19 @@ const TermKeywordImageChoice = ({ term }: Props) => {
         >
           Search by image
         </label>
+        {errors.name?.type === "required" && (
+          <span className="term-keyword-error">Enter search query.</span>
+        )}
       </form>
+      <div className="flex flex-wrap">
+        {searchedImages.map((imageUrl: string, index) => {
+          return (
+            <div className="basis-1/4" key={`${index}-${imageUrl}`}>
+              <img className="max-w-full h-auto" src={imageUrl} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
