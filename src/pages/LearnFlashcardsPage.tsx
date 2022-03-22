bffbe1419bae166b85047541352071065b01eb78
Shysh-Oleksandr <sasha.shysh23@gmail.com>
2022-03-22 13:25:03 +0200

    Made bg cards.

--- src/pages/LearnFlashcardsPage.tsx
+++ src/pages/LearnFlashcardsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { bindActionCreators } from "redux";
@@ -9,6 +9,19 @@ import { actionCreactors, State } from "../state";
 import "../styles/flashcards.css";
 
 type Props = {};
+
+const MAX_BG_CARDS = 5;
+
+const calcTermsLeft = (
+  currentTermIndex: number,
+  termsLength: number
+): number => {
+  const termLeft =
+    termsLength - (currentTermIndex + 1) <= MAX_BG_CARDS
+      ? termsLength - (currentTermIndex + 1)
+      : MAX_BG_CARDS;
+  return termLeft;
+};
 
 const LearnFlashcardsPage = (props: Props) => {
   const [currentTermIndex, setCurrentTermIndex] = useState<number>(0);
@@ -27,23 +40,57 @@ const LearnFlashcardsPage = (props: Props) => {
   const currentTerm = shuffledTerms[currentTermIndex];
   const termsLength = shuffledTerms.length;
   const cardRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+  let termsLeftNumber = calcTermsLeft(currentTermIndex, termsLength);
+
+  const termsLeft = new Array(termsLeftNumber).fill(0);
+
+  const setCardTransform = (newTermIndex: number) => {
+    termsLeftNumber = calcTermsLeft(newTermIndex, termsLength);
+
+    const rotateDegrees =
+      termsLeftNumber >= MAX_BG_CARDS
+        ? 10
+        : 10 + (MAX_BG_CARDS - termsLeftNumber);
+    const bottomPx =
+      termsLeftNumber >= MAX_BG_CARDS
+        ? 137
+        : 137 - (MAX_BG_CARDS - termsLeftNumber) * 11;
+
+    cardRef.current.style.transform =
+      currentTermIndex < newTermIndex
+        ? `translateX(50%) rotateY(-50deg) rotateX(${rotateDegrees}deg) translateZ(0)`
+        : `translateX(-50%) rotateY(50deg) rotateX(${rotateDegrees}deg) translateZ(0)`;
+    cardRef.current.style.transition = "transform 0.05s ease";
+    cardRef.current.style.bottom = `${bottomPx}px`;
+
+    setTimeout(() => {
+      cardRef.current.style.transition = "transform 0.24s ease";
+      cardRef.current.style.transform = `rotateX(${rotateDegrees}deg)`;
+    }, 100);
+  };
 
   const changeCurrentTerm = (newTermIndex: number) => {
     setCurrentTermIndex(newTermIndex);
     setIsCurrentSideFront(isStartSideFront);
     setShowDefinition(false);
 
-    cardRef.current.style.transform =
-      currentTermIndex < newTermIndex
-        ? "translateX(50%) rotateY(-50deg) translateZ(0)"
-        : "translateX(-50%) rotateY(50deg) translateZ(0)";
-    cardRef.current.style.transition = "transform 0.05s ease";
-
-    setTimeout(() => {
-      cardRef.current.style.transition = "transform 0.24s ease";
-      cardRef.current.style.transform = "none";
-    }, 100);
+    setCardTransform(newTermIndex);
   };
+
+  const shuffleTerms = () => {
+    setShuffledTerms(shuffle(shuffledTerms));
+
+    setCurrentTermIndex(0);
+    isCurrentSideFront || !isStartSideFront
+      ? setShowDefinition(!showDefinition)
+      : setIsCurrentSideFront(isStartSideFront);
+
+    setCardTransform(0);
+  };
+
+  useEffect(() => {
+    setCardTransform(0);
+  }, []);
 
   return (
     <div className="learn-cards flex h-full mt-4 justify-between">
@@ -71,17 +118,7 @@ const LearnFlashcardsPage = (props: Props) => {
             </span>
           </div>
           <div className="mt-80">
-            <button
-              onClick={() => {
-                setShuffledTerms(shuffle(shuffledTerms));
-
-                setCurrentTermIndex(0);
-                isCurrentSideFront || !isStartSideFront
-                  ? setShowDefinition(!showDefinition)
-                  : setIsCurrentSideFront(isStartSideFront);
-              }}
-              className="btn block w-full mb-4"
-            >
+            <button onClick={shuffleTerms} className="btn block w-full mb-4">
               Shuffle
             </button>
             <button className="btn block w-full">Settings</button>
@@ -90,7 +127,7 @@ const LearnFlashcardsPage = (props: Props) => {
       </div>
       <div className="learn-cards-content basis-5/6 relative">
         <div
-          className="cards absolute bottom-16 top-0 left-0 right-0"
+          className="cards z-40 absolute bottom-[137px] top-0 left-0 right-0"
           ref={cardRef}
         >
           <TermCard
@@ -100,6 +137,19 @@ const LearnFlashcardsPage = (props: Props) => {
             setIsCurrentSideFront={setIsCurrentSideFront}
             isCurrentSideFront={isCurrentSideFront}
           />
+        </div>
+        <div className="bg-cards absolute bottom-20 top-0 left-0 right-0">
+          {termsLeft.map((term, index) => {
+            return (
+              <div
+                key={`${currentTerm.id}-bg-card-${index}`}
+                style={{ bottom: `${64 + index * 12}px` }}
+                className={`bg-slate-700 h-full w-full rounded-xl absolute left-0 border-solid border-b-4 p-8 border-slate-400 text-5xl flex items-center justify-center`}
+              >
+                {shuffledTerms[currentTermIndex + 1].term}
+              </div>
+            );
+          })}
         </div>
         <div className="navigation flex items-center justify-center absolute bottom-0 left-1/2 -translate-x-1/2">
           <button
